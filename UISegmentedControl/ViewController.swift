b2839f243930b471debd067951bcd13420369888
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-29 23:43:16 +0900

    first commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by 中田誠 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
