ecd390f2079a1c8f296689bd32889be7bb0acce2
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-29 23:59:44 +0900

    UISegmentedContro

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func changeSelect(_ sender: Any) {
+        let num = segmentedControl.selectedSegmentIndex
+        if let title = segmentedControl.titleForSegment(at: num) {
+            label.text = "\(num)番目の\(title)が選ばれた"
+        }
+    }
+    
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        segmentedControl.selectedSegmentIndex = 1
+    }
+    
 }
 
